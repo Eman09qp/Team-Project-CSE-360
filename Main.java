8ba49e065335142ed3b95014c332356919cf0092
Eman09qp <99450870+Eman09qp@users.noreply.github.com>
2022-03-28 21:36:45 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,4 +1,5 @@
 package com.CatJam;
+import java.util.LinkedList;
 
 public class Main {
 
@@ -10,6 +11,7 @@ public class Main {
                 "abigail123@hotmail.com",
                 "abyWill",
                 "securePassword123",
+                1234,
                 "Rural road",
                 "Tempe",
                 "Arizona",
@@ -21,6 +23,7 @@ public class Main {
                 "carlosPerez@hotmail.com",
                 "carlitosP",
                 "iDontKnow456",
+                3456,
                 "Eastern Ave",
                 "Phoenix",
                 "Arizona",
@@ -28,5 +31,19 @@ public class Main {
 
         System.out.println(Abigail.toString());
         System.out.println(Carlos.toString());
+        Food Pizza = new Food("Dominoes Pizza",
+                "American?",
+                "Flour, tomato sauce, pepperoni, mozzarella cheese",
+                5.99);
+        Food Hamburger = new Food("Hamburger",
+                "American",
+                "Bread, quarter pound beef, bacon, lettuce, tomato, cheese",
+                4.59);
+
+        Carlos.cart.add(Pizza);
+        Carlos.cart.add(Hamburger);
+        System.out.print("Cart: ");
+        Carlos.printCart();
     }
 }
+
