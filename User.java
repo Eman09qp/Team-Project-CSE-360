572149a4254694d41687fa6d4b17dcdb7aa850e1
Eman09qp <99450870+Eman09qp@users.noreply.github.com>
2022-03-29 11:47:28 -0700

    Update User.java
    oops

--- User.java
+++ User.java
@@ -1,34 +1,90 @@
 
-public class Staff extends User {
+public abstract class User {
 
-    // Staff constructor
-    public Staff(String staffFirstName,
-                 String staffLastName,
-                 String staffPhoneNumber,
-                 String staffEmail,
-                 String staffUsername,
-                 String staffPassword,
-                 int staffApartmentNumber,
+    protected String firstName = "";
+    protected String lastName = "";
+    protected String phoneNumber = "";
+    protected String email = "";
+    protected String userName = "";
+    private String password = "";
+    Address userAddress;
+
+    // Abstract constructor //
+    public User (String newFirstName,
+                 String newLastName,
+                 String newPhoneNumber,
+                 String newEmail,
+                 String newUserName,
+                 String newPassword,
+                 int newApartmentNumber,
                  String newStreet,
                  String newCity,
                  String newState,
                  int newZipcode)
     {
-        super(staffFirstName,
-                staffLastName,
-                staffPhoneNumber,
-                staffEmail,
-                staffUsername,
-                staffPassword,
-                staffApartmentNumber,
-                newStreet,
-                newCity,
-                newState,
-                newZipcode);
+        firstName = newFirstName;
+        lastName = newLastName;
+        phoneNumber = newPhoneNumber;
+        email = newEmail;
+        userName = newUserName;
+        password = newPassword;
+        userAddress = new Address(newApartmentNumber, newStreet, newCity, newState, newZipcode);
     }
 
-    //Sets the frequent boolean as true in the Customer class
-    public void giveCoupon(Customer frequentCustomer){
-        frequentCustomer.isFrequentCustomer();
+
+    public String toString() {
+        return  "\nUserName:\t\t\t" + userName +
+                "\nName:\t\t\t" + firstName + " " + lastName +
+                "\nPhone number:\t\t\t" + phoneNumber +
+                "\nAddress:\t\t\t" + userAddress.getAddress() +
+                "\nEmail:\t\t\t" + email +
+                "\nPassword:\t\t\t" + this.getPassword();
     }
+
+    // Getter and Setter methods
+    public String getFullName(){ return firstName + " " + lastName; }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String newFirstName) { this.firstName = newFirstName; }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String newLastName) { this.lastName = newLastName; }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() { return email; }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
 }
+
